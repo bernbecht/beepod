a0496de0d58118f2b2b2ae1a7c3c2663a91b5aed
Bernardo Bechtold Pereira <bechtold.bernardo@gmail.com>
2018-06-18 16:57:08 +0200

    adds subscription list prototype

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HeaderBar from './components/HeaderBar/HeaderBar';
+import SubscribedList from './components/SubscribedList/SubscribedList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HeaderBar />
+        <SubscribedList />
       </div>
     );
   }
---
+++ src/components/SubscribedList/SubscribedList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './SubscribedList.css';
+
+
+class SubscribedList extends Component {
+    render() {
+        return (
+            <div className='SubscribedList'>
+                <div className="horizontalList">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SubscribedList;
